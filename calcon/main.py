74b45da86b47879dd7b2d738f6d9f9b2d9aff12a
Ederic Oytas <edericoytas@gmail.com>
2023-12-29 20:28:56 -0800

    Add expr argument; add main doc

--- calcon/main.py
+++ calcon/main.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 import typer
 
 
@@ -5,5 +6,16 @@ app = typer.Typer()
 
 
 @app.command(no_args_is_help=True)
-def main() -> None:
+def main(
+    expr: Annotated[
+        str,
+        typer.Argument(
+            metavar="EXPR",
+            help="Expression to evaluate.",
+            show_default=False,
+        ),
+    ],
+) -> None:
+    """Calculator app with physical quantities."""
+
     print("Hello world!")
