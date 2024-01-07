650c52edcd44afc208d56ed9dd67fcfb255365e4
Ederic Oytas <edericoytas@gmail.com>
2024-01-06 21:56:50 -0800

    Fix parse debug script

--- tests/debug.py
+++ tests/debug.py
@@ -1,6 +1,7 @@
 """Debug script for testing purposes."""
 
 
+import lark
 import typer
 
 from calcon.parsing import parse_expr
@@ -12,7 +13,10 @@ app = typer.Typer(no_args_is_help=True)
 @app.command(no_args_is_help=True)
 def parse(expr: str):
     """Parses the given expression and prints the parse tree."""
-    print(parse_expr(expr).pretty())
+    parser = lark.Lark.open("calcon/grammar.lark", start="expr")
+    tree = parser.parse(expr)
+
+    print(tree.pretty())
 
 
 if __name__ == "__main__":
