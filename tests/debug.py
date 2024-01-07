1067401374b43a167db38047f8c28fdd4aaac89b
Ederic Oytas <edericoytas@gmail.com>
2024-01-07 01:36:45 -0800

    Add start parameter

--- tests/debug.py
+++ tests/debug.py
@@ -9,9 +9,9 @@ app = typer.Typer(no_args_is_help=True)
 
 
 @app.command(no_args_is_help=True)
-def parse(expr: str):
+def parse(expr: str, start: str = "expr"):
     """Parses the given expression and prints the parse tree."""
-    parser = lark.Lark.open("calcon/grammar.lark", start="expr")
+    parser = lark.Lark.open("calcon/grammar.lark", start=start)
     tree = parser.parse(expr)
 
     print(tree.pretty())
