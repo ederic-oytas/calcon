00ab53b98c922155060b37d5fac27077baafcf3a
Ederic Oytas <edericoytas@gmail.com>
2024-01-09 09:06:23 -0800

    Implement comments

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,11 +1,13 @@
 """Testing module for parsing.py"""
 
-from calcon.parsing import parse_statements
+from calcon.expressions import Expression
+from calcon.parsing import parse_expr, parse_statements
 
 
 def test_statements() -> None:
     assert len(parse_statements("")) == 0
     assert len(parse_statements("\n\n\n\n")) == 0
+    assert len(parse_statements("#comment")) == 0
     assert len(parse_statements("1 liter = 0.001 m^3")) == 1
     assert len(parse_statements("1 liter (L) = 0.001 m^3")) == 1
     assert len(parse_statements("1 liter [litre] = 0.001 m^3")) == 1
@@ -18,10 +20,10 @@ def test_statements() -> None:
         """
 
         1 inch (in) [inches] = 25.4 mm
-
+        # comment line
 
         1 liter (L) [litre, litres] = 0.001 m^3
-
+        # comment line
 
         """
     )
@@ -30,8 +32,14 @@ def test_statements() -> None:
     statements = parse_statements(
         """
         1 inch (in) [inches] = 25.4 mm
-        1 foot (ft) [feet] = 12 in
+        1 foot (ft) [feet] = 12 in  # comment
         1 liter (L) [litre, litres] = 0.001 m^3
         """
     )
     assert len(statements) == 3
+
+
+def test_expr() -> None:
+    assert isinstance(parse_expr("5"), Expression)
+    assert isinstance(parse_expr("5  # comment"), Expression)
+    assert isinstance(parse_expr("5 meters  # comment"), Expression)
